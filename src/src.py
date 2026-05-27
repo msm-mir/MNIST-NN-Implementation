a77d08e502f5503a3cbe99efd8395f0726d8cdbd
marble <msm.mirdamad@gmail.com>
2026-05-27 23:28:02 +0330

    create neural network class

--- src/src.py
+++ src/src.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+class neural_network:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
 # read dataset
 train_df = pd.read_csv("src/data/mnist_train.csv")
 test_df = pd.read_csv("src/data/mnist_test.csv")
@@ -20,8 +25,12 @@ X_test = X_test.T / 255
 # weights and biases initialization
 np.random.seed(42)
 
-W1 = np.random.randn(128, X_train.shape[0]) * 0.01
-b1 = np.zeros((128, 1))
+# first hidden layer
+neuron1 = 128
+Weight1 = np.random.randn(neuron1, X_train.shape[0]) * 0.01
+bias1 = np.zeros((neuron1, 1))
 
-W2 = np.random.randn(10, 128) * 0.01
-b2 = np.zeros((10, 1))
+# output layer
+neuron2 = 10
+Weight2 = np.random.randn(neuron2, neuron1) * 0.01
+bias2 = np.zeros((neuron2, 1))
